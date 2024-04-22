05db51c7fb0ebe84e69e75b41786fd7acddb25bf
KhuiHihi <khoikhuat0807@gmail.com>
2024-04-22 20:01:30 +0700

    final commit plz

--- src/index.js
+++ src/index.js
@@ -4,24 +4,11 @@ import "./index.css";
 import App from "./App";
 import Sponsors from "./components/Sponsors/Sponsors";
 import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
-import store from "./redux/store";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <Provider store={store}>
-    {/* <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<App />} />
-          <Route path="speakers" element={<Sponsors />} />
-        </Route>
-      </Routes>
-    </BrowserRouter> */}
-    <App />
-  </Provider>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
